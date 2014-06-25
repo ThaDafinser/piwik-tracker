375a1bde06a23e4ea5c27bad70af5e2128408f8e
Martin Keckeis <martin.keckeis1@gmail.com>
2014-06-25 11:24:17 +0200

    Update TrackerTest.php

--- tests/PiwikPhpTrackerTest/TrackerTest.php
+++ tests/PiwikPhpTrackerTest/TrackerTest.php
@@ -9,9 +9,20 @@ use PiwikPhpTracker\Tracker;
  */
 class TrackerTest extends PHPUnit_Framework_TestCase
 {
-    public function testCanCreate(){
-        $tracker = new Tracker(1);
-        
+
+    public function testCanCreate()
+    {
+        $tracker = new Tracker('http://example.com', 1);
         $this->assertInstanceOf('PiwikPhpTracker\Tracker', $tracker);
+        $this->assertInstanceOf('PiwikPhpTracker\Parameters', $tracker);
+    }
+
+    public function testIdSite()
+    {
+        $tracker = new Tracker('http://example.com', 1);
+        $this->assertEquals(1, $tracker->getIdSite());
+        
+        $tracker->setIdSite(25);
+        $this->assertEquals(25, $tracker->getIdSite());
     }
 }
